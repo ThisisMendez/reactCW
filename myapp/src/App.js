4a3df327c70e329d87a42eac6b80516cdb57e53b
Alexis Mendez <alexis.mendezpvd@gmail.com>
2020-03-27 16:38:48 -0400

    props

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -6,7 +6,8 @@ class App extends Component {
     return (
       <div className="App">
         <h1>My first React app</h1>
-        <Ninjas />
+        <Ninjas name="Ryu" age="25" belt="black"/>
+        <Ninjas name="Yoshi" age="30" belt="green"/>
       </div>
     );
   }
