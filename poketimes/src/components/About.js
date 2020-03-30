9e6fc4ac9494d1f06a67133130a0a510fc079f81
Alexis Mendez <alexis.mendezpvd@gmail.com>
2020-03-30 13:27:42 -0400

    navbar

---
+++ poketimes/src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About =() => { 
+  return ( 
+    <div className="container">
+      <h4 className="center">About</h4>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam lorem elit, mattis non commodo ultrices, sagittis vitae nisl. Praesent imperdiet. </p>
+    </div>
+  )
+}
+
+export default About 
---
+++ poketimes/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home =() => { 
+  return ( 
+    <div className="container">
+      <h4 className="center">Home</h4>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam lorem elit, mattis non commodo ultrices, sagittis vitae nisl. Praesent imperdiet. </p>
+    </div>
+  )
+}
+
+export default Home 
